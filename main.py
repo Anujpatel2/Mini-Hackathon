51276d68286bee92402c670e6a3abd6dba07cfc1
Software Project Management <anujmaitri@gmail.com>
2020-03-04 10:57:43 -0500

    added everything

--- main.py
+++ main.py
@@ -4,7 +4,34 @@ import requests
 
 def main():
     cocktail_by_name = get_cocktail_by_name()
-    print(cocktail_by_name)
+
+    for key in cocktail_by_name.keys():
+        for items in cocktail_by_name[key]:
+            for key2 in items:
+                print(key2 + ": " +str(items[key2]))
+
+    print("***********************************************************")
+
+    ingredients_by_name=get_ingredients_by_name()
+    for key in ingredients_by_name.keys():
+        for items in ingredients_by_name[key]:
+            for key2 in items:
+                print(key2 + ": " + str(items[key2]))
+
+    print("***********************************************************")
+
+
+    full_details=get_full_cocktail_details()
+
+    for key in full_details.keys():
+        for items in full_details[key]:
+            for key2 in items:
+                print(key2 + ": " + str(items[key2]))
+
+    print("***********************************************************")
+
+    print(get_random_cocktail())
+
 
 
 
@@ -25,6 +52,80 @@ def get_cocktail_by_name():
         return json.loads(response.content.decode('utf-8'))
     else:
         return None
+
+def get_ingredients_by_name():
+    ingredient = str(input("Enter the name of the ingredient: "))
+
+    api_token = 1
+    api_url_base = "https://www.thecocktaildb.com/api/json/v1/1/search.php?i=" + ingredient
+
+    headers = [u'ingredients']
+
+    api_url = format(api_url_base)
+
+    response = requests.get(api_url_base)
+
+    if response.status_code == 200:
+        return json.loads(response.content.decode('utf-8'))
+    else:
+        return None
+
+def get_full_cocktail_details():
+    fullDetails = str(input("Enter the name of the cocktail: "))
+
+    api_token = 1
+    api_url_base = "https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=" + fullDetails
+
+    headers = [u'ingredients']
+
+    api_url = format(api_url_base)
+
+    response = requests.get(api_url_base)
+
+    if response.status_code == 200:
+        return json.loads(response.content.decode('utf-8'))
+    else:
+        return None
+
+def get_random_cocktail():
+
+    api_token = 1
+    api_url_base = "https://www.thecocktaildb.com/api/json/v1/1/random.php"
+
+    headers = [u'ingredients']
+
+    api_url = format(api_url_base)
+
+    response = requests.get(api_url_base)
+
+    if response.status_code == 200:
+        return json.loads(response.content.decode('utf-8'))
+    else:
+        return None
+
+def get_ingredient():
+    ingredient = str(input("Enter the name of the ingredient: "))
+
+    api_token = 1
+    api_url_base = "https://www.thecocktaildb.com/api/json/v1/1/filter.php?i="+ingredient
+
+    headers = [u'ingredients']
+
+    api_url = format(api_url_base)
+
+    response = requests.get(api_url_base)
+
+    if response.status_code == 200:
+        return json.loads(response.content.decode('utf-8'))
+    else:
+        return None
+
+
+
+
+
+
+
 
 
 
